4a1555b0bb02ed0907b8a2522f6543c62beb4baf
andrewgodman <andrew@godman.io>
2018-10-23 16:40:42 +1100

    added session handling to allow user to login

--- main.rb
+++ main.rb
@@ -1,6 +1,27 @@
-     
 require 'sinatra'
 require 'pg'
+
+require 'sinatra/reloader' 
+require 'pry'
+
+require_relative 'db_config'
+require_relative 'models/user'
+
+enable :sessions
+
+helpers do
+  def logged_in?
+    if current_user
+      return true
+    else
+      return false
+    end
+  end
+  
+  def current_user
+    User.find_by(id:session[:user_id])
+  end
+end
 
 get '/' do
   erb :index
@@ -8,6 +29,17 @@ end
 
 get '/login' do
   erb :login
+end
+
+post '/session' do
+  user = User.find_by(email: params[:email])
+  if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    # try again
+  end
+  redirect to('/')
 end
 
 get '/signup' do
