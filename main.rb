f0dc7c93fead8fd6394431068a726e62534f6fe0
andrewgodman <andrew@godman.io>
2018-10-25 16:04:29 +1100

    fixed name for prod deployment

--- main.rb
+++ main.rb
@@ -9,7 +9,7 @@ require_relative 'db_config'
 require_relative 'models/user'
 require_relative 'models/groupactivity'
 require_relative 'models/attendingactivity'
-require_relative 'models/ActivityCategory'
+require_relative 'models/activitycategory'
 
 enable :sessions
 
