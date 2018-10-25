e0c0d6cd13b0930c4dd2b16ee590d9c392b1c8de
andrewgodman <andrew@godman.io>
2018-10-25 12:52:19 +1100

    added new categories in to the seed file

--- seed.sql
+++ seed.sql
@@ -1,2 +1,3 @@
 INSERT INTO users (email, user_name, first_name, last_name, password_digest) VALUES ('andrew@example.com', 'andrew', 'andrew', 'godman', '');
-INSERT INTO 
+INSERT INTO activity_categories (name) VALUES ('run');
+INSERT INTO activity_categories (name) VALUES ('cycle');
